f6de6041f890d8734b00c00b35396783a5566806
Ivan Luchev <luchevz@gmail.com>
2021-01-02 14:43:45 +0000

    Fix car rotation and position when drawing

--- src/js/canvas.js
+++ src/js/canvas.js
@@ -4,27 +4,30 @@ export class Canvas {
         this.canvas = document.getElementById( canvasId );
         this.ctx = this.canvas.getContext( '2d' );
         this.carImages = carImages;
+        for ( let car of this.carImages ) {
+            car.width /= 4;
+            car.height /= 4;
+        }
         this.trackImages = trackImages;
-        carImages[0].width /= 4;
-        carImages[0].height /= 4;
+    }
+
+    clear() {
+        this.ctx.clearRect( 0, 0, this.canvas.width, this.canvas.height );
     }
 
     drawCar( car ) {
-        this.ctx.save();
+        this.ctx.setTransform( 1, 0, 0, 1, 0, 0 );
         let width = this.carImages[car.imageId].width;
         let height = this.carImages[car.imageId].height;
-        // this.ctx.translate( car.x + width / 2, car.y + height / 2);
-        this.ctx.rotate( car.rotation );
-        // this.ctx.translate( - width / 2, - height / 2);
-        this.ctx.drawImage( this.carImages[car.imageId], -width / 2, -height / 2, width, height);
-        this.ctx.restore();
+        this.ctx.translate( car.x, car.y );
+        this.ctx.rotate( car.rotation + Math.PI / 2 );
+        this.ctx.drawImage( this.carImages[car.imageId], - width / 2, - height / 2, width, height );
     }
 
-    drawTrack(trackIndex) {
-        this.ctx.save();
+    drawTrack( trackIndex ) {
+        this.ctx.setTransform( 1, 0, 0, 1, 0, 0 );
         this.canvas.width = this.trackImages[trackIndex].width;
         this.canvas.height = this.trackImages[trackIndex].height;
         this.ctx.drawImage( this.trackImages[trackIndex], 0, 0 );
-        this.ctx.restore();
     }
 }
