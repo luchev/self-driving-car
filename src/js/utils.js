b398fc7e0caf012bf57c4ca70975734a456ce926
luchev <luchevz@gmail.com>
2021-02-11 12:22:41 +0200

    Add random float function

--- src/js/utils.js
+++ src/js/utils.js
@@ -2,6 +2,10 @@ export function getRandomInteger( min, max ) {
   return Math.floor( ( max - min ) * Math.random() ) + min;
 }
 
+export function getRandomFloat( min, max ) {
+  return ( max - min ) * Math.random() + min;
+}
+
 export function getRandomIntegers( min, max, numIntegers ) {
   const output = [];
   for ( let i = 0; i < numIntegers; ++i ) {
